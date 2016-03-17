a6d26ca4a29d1e3e2f0d36dcd8594638df3a1e77
manu <sefirot_cloud@hotmail.com>
2016-03-17 11:35:11 +0100

    Jobeet day 3 completed

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="categories")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Job", mappedBy="category")
+     */
+    private $jobs;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Affiliate", mappedBy="categories")
+     */
+    private $affiliates;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setJobs($jobs)
+    {
+        $this->jobs = $jobs;
+
+        return $this;
+    }
+
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    public function setAffiliates($affiliates)
+    {
+        $this->affiliates = $affiliates;
+
+        return $this;
+    }
+
+    public function getAffiliates()
+    {
+        return $this->affiliates;
+    }
+
+    public function __toString()
+    {
+        return $this->getName() ? $this->getName() : "";
+    }
+}
